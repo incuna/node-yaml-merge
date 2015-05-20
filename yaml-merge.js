96dc0c51d0e9c5480ab79b130efdc73114931e6f
Graham Gilchrist <git@grahamgilchrist.com>
2015-05-20 16:02:38 +0100

    Initial commit with package and first method

---
+++ yaml-merge.js
@@ -0,0 +1,33 @@
+module.exports = function () {
+    'use strict';
+
+    var _ = require('lodash');
+    var gruntFile = require('grunt/lib/grunt/file');
+
+    /**
+     * Returns a merged set of yaml files
+     * @param {array} files - array of file paths to merge together into one object.
+     * @return {object} Merged javascript object containing converted data from the merged yaml files
+     */
+    var mergeFiles = function (files) {
+        if (!_.isArray(files)) {
+            throw new Error('Arguments to config-helper.mergeConfig should be an array');
+        }
+
+        var appConfig = {};
+        files.forEach(function (filePath) {
+            if (gruntFile.exists(filePath)) {
+                var fileConfig = gruntFile.readYAML(filePath);
+                // Use lodash to do a 'deep merge' which only overwrites the properties
+                // specified in previous config files, without wiping out their child properties.
+                _.merge(appConfig, fileConfig);
+            }
+        });
+
+        return appConfig;
+    };
+
+    return {
+        mergeFiles: mergeFiles
+    };
+};
